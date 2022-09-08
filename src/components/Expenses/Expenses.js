b201f755629bd543c7be10d0d5373b5a560e2992
Musteqeem <soladoyemustekeem@gmail.com>
2022-09-08 14:55:57 +0100

    Rendering list of data with the use of map

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,7 +6,6 @@ import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 
 const Expense =(props)=> {
-   const items = props.items;
    
    const [filteredYear, setFilteredYear] = useState('2020');
    const filterChangeHandler = (enteredYear) => {
@@ -17,22 +16,12 @@ const Expense =(props)=> {
        <div>
           <Card className="expenses">
              <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-             <ExpenseItem
-                title={items[0].title}
-                amount={items[0].amount}
-                date={items[0].date}/>
-             <ExpenseItem
-                title={items[1].title}
-                amount={items[1].amount}
-                date={items[1].date}/>
-             <ExpenseItem
-                title={items[2].title}
-                amount={items[2].amount}
-                date={items[2].date}/>
-             <ExpenseItem
-                title={items[3].title}
-                amount={items[3].amount}
-                date={items[3].date}/>
+             {props.items.map(expense => {
+                return <ExpenseItem
+                   title={expense.title}
+                   amount={expense.amount}
+                   date={expense.date} />
+             })}
            </Card>
        </div>
     );
